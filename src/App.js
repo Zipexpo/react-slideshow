102321fd3c0081f4c781192420f6a8b55187ce6d
Zipexpo <nvtngan@hcmus.edu.vn>
2024-10-23 11:18:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ import SlideShow from './components/SlideShow';
 import { useState } from 'react';
 
 function App() {
-  const [obj,setObj] = useState({title:"SlideShow", version:"1.0"});
-
-  const handleChangeVersion = ()=>{
-    delete obj.version;
-    setObj({...obj});
-  }
   return (
     <div className="App">
-        <h2 style={{textAlign:"center"}}>{obj.title} v{obj.version}</h2>
-        <button onClick={handleChangeVersion}>Delete version</button>
         <SlideShow/>
     </div>
   );
